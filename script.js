de7f2c0aa9095bd882f639b3a87c9ec76db3df9e
rigole <foplacide@gmail.com>
2021-06-06 11:56:39 +0100

    getting the unsplash API parameters

---
+++ script.js
@@ -0,0 +1,5 @@
+// unsplash API parameters
+
+const apiKey = 'Y_gvnN-EBXyNfj0SfCiFevAwImT0ehAvgvX06GHsDW0';
+const count = 10;
+const urlAPI =  `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
