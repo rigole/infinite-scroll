475f118d4b430c1ea33df2f2cbe6cb35bbae368f
rigole <foplacide@gmail.com>
2021-06-06 12:13:54 +0100

    fetching data from the API

--- script.js
+++ script.js
@@ -3,3 +3,18 @@
 const apiKey = 'Y_gvnN-EBXyNfj0SfCiFevAwImT0ehAvgvX06GHsDW0';
 const count = 10;
 const urlAPI =  `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+//Get photos from unsplash API
+
+async function getPhotos() {
+    try{
+        const response = await fetch(urlAPI);
+        const data = await response.json();
+        console.log(data);
+        
+    }catch (e) {
+        
+    }
+}
+
+getPhotos();
