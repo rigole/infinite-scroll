0259ad8ed0ea30f9f0fc92de1e75b512c42c0ae8
rigole <foplacide@gmail.com>
2021-06-06 16:52:22 +0100

    create element a and img for the new photos called by the API

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ const urlAPI =  `https://api.unsplash.com/photos/random/?client_id=${apiKey}&cou
 
 //Get photos from unsplash API
 function displayPhotos() {
-    photoArray.forEach((photo)=>{
+    photoArray.forEach((photo)=> {
       // create <a> element link to unspleash
         const item = document.createElement('a');
-        item.setAttribute('href',photo.link.html);
+        item.setAttribute('href',photo.links.html);
         item.setAttribute('target', '_blank');
 
       // create <img> for photo
@@ -19,15 +19,20 @@ function displayPhotos() {
       img.setAttribute('src', photo.urls.regular);
       img.setAttribute('alt', photo.alt_description);
       img.setAttribute('title', photo.alt_description);
+
+      //put <a> inside <img> and then put both inside the div image container
+        item.appendChild(img);
+        imageContainer.appendChild(item);
     });
 }
+
 // Create Elements for Link,Photos, Add to DOM
 async function getPhotos() {
     try{
         const response = await fetch(urlAPI);
         photoArray = await response.json();
-        //console.log(photoArray);
-        displayPhotos();
+        console.log(photoArray);
+       displayPhotos();
     }catch (e) {
         
     }
